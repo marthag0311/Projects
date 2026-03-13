3f715b1899d0173b829c772fa8b860afeaf3067b
marthag0311 <marageofrey@gmail.com>
2026-03-13 05:46:42 +0300

    Add files via upload

---
+++ AI Projects/Scheduling Optimization/Assignments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Exam_Project
+{
+    public class Assignments
+    {
+        public string Assignment { get; set; }
+        public double Priority { get; set; }
+        public double Complexity { get; set; }
+        public double Workload { get; set; }
+        public double PredictedDuration { get; set; }        
+    }
+}

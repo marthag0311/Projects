d8d48cc297fb08a056d1a0e5932dc84a43d57736
marthag0311 <marageofrey@gmail.com>
2026-06-20 21:52:11 +0300

    Updated ddl_bronze

--- Data Engineer Projects/Sales Data Warehouse/scripts/bronze/ddl_bronze.sql
+++ Data Engineer Projects/Sales Data Warehouse/scripts/bronze/ddl_bronze.sql
@@ -12,24 +12,28 @@ Script Purpose:
 IF OBJECT_ID ('bronze.employees', 'U') IS NOT NULL
 	DROP TABLE bronze.employees;
 CREATE TABLE bronze.employees (
+[employee_key] INT,
 [employee] NVARCHAR(50), 
 [employee_position] NVARCHAR(50),
 [employee_phone1] NVARCHAR(20),
-[employee_phone2] NVARCHAR(20)
+[employee_phone2] NVARCHAR(20),
+[employee_gender] NVARCHAR(20)
 );
 
 IF OBJECT_ID ('bronze.salaries', 'U') IS NOT NULL
 	DROP TABLE bronze.salaries;
 CREATE TABLE bronze.salaries (
-[salary_date] DATETIME,
-[salary_employee] NVARCHAR(50), 
+[salary_key] INT,
+[salary_date] DATE,
+[employee_key] NVARCHAR(50), 
 [salary] DECIMAL(10,2)
 );
 
 IF OBJECT_ID ('bronze.expenses', 'U') IS NOT NULL
 	DROP TABLE bronze.expenses;
 CREATE TABLE bronze.expenses (
-[expense_date] DATETIME,
+[expense_key] INT,
+[expense_date] DATE,
 [expense_type] NVARCHAR(50), 
 [expense_category] NVARCHAR(50), 
 [expense_amount] DECIMAL(10,2)
@@ -38,7 +42,9 @@ CREATE TABLE bronze.expenses (
 IF OBJECT_ID ('bronze.sales', 'U') IS NOT NULL
 	DROP TABLE bronze.sales;
 CREATE TABLE bronze.sales (
-[sale_date] DATETIME,
+[sale_key] INT,
+[sale_date] DATE,
 [sale_service] NVARCHAR(200), 
-[sale_price] DECIMAL(10,2)
+[sales] DECIMAL(10,2),
+[employee_key] NVARCHAR(50)
 );
